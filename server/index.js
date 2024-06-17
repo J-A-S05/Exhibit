15b283b3879c4a88134c2f11efa62791062882d3
J-A-S05 <100374150+J-A-S05@users.noreply.github.com>
2024-06-17 16:05:25 +0530

    cors settings changed

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Enable credentials (cookies, authorization headers)
 };
 
